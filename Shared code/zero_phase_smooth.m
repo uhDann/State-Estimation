db7778bd16e2394b7134047bc9502dbb7629c443
misharuko <>
2025-03-30 21:01:02 +0100

    Fixed smoothing again

--- Shared code/zero_phase_smooth.m
+++ Shared code/zero_phase_smooth.m
@@ -1,4 +1,4 @@
-function smoothed = zero_phase_smooth(data, order, cutoff)
+function smoothed = zero_phase_smooth(data, order, cutoff, sample_frequency)
 % Smooths each column of an NxM array with zero-phase filtering.
 %
 % Rewrite docstring to be like other files
@@ -9,16 +9,18 @@ function smoothed = zero_phase_smooth(data, order, cutoff)
 %       Order of the Butterworth filter.
 %   cutoff: float
 %       Cutoff frequency of the Butterworth filter.
+%   sample_frequency: float
+%       Sample frequency of the data.
 % Inputs:
 %   data: NxM
 %       Array that may have step-like behavior.
 %
-% Outputs:
+% Returns:
 %   smoothed: NxM
 %       Array where each column has been smoothed with a zero-phase Butterworth low-pass filter.
 
 % Design Butterworth low-pass filter.
-[b, a] = butter(order, cutoff);
+[b, a] = butter(order, cutoff/(sample_frequency/2), "low");
 
 smoothed = zeros(size(data));
 
