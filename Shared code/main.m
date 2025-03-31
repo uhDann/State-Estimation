1d250eb50f6b9e59863223c889bac7b7c905117c
misharuko <>
2025-03-31 17:55:42 +0100

    Added some plotting stuff

--- Shared code/main.m
+++ Shared code/main.m
@@ -12,7 +12,7 @@ GT_Time = out.Sensor_Time.time;
 GT_position = squeeze(out.GT_position.signals.values);
 GT_rotation = squeeze(out.GT_rotation.signals.values);
 
-GT_heading = quat2eul(GT_rotation, 'ZYX'); 
+GT_heading = quat2eul(GT_rotation, 'ZYX');
 GT_heading = unwrap(GT_heading(:,1));
 
 ToF1 = out.Sensor_ToF1.signals.values;
@@ -25,9 +25,38 @@ all_ToF = calibrate_ToF([ToF1(:, 1), ToF2(:, 1), ToF3(:, 1)]);
 
 [X_est, P_Est, GT] = EKF(out);
 % X_est = zero_phase_smooth(X_est, 4, 0.5, 200);
-plot_trajectory(GT_Time, GT_position, X_est(:, 1:2), X_est(:, 3), all_ToF, true);
+% plot_trajectory(GT_Time, GT_position, X_est(:, 1:2), X_est(:, 3), all_ToF, true);
 
 pos_est = ToF_mag_to_meas(all_ToF, GT_heading);
 % plot_trajectory(GT_Time, pos_est, GT_heading, all_ToF, true);
 
-[RMSE, metrics] = evaluateTrajectory(X_est, out);
+% [RMSE, metrics] = evaluateTrajectory(X_est, out);
+
+plot_var_xy = 2;
+
+figure;
+hold on;
+
+% Plot GT_position and X_est on the left y-axis
+yyaxis left;
+plot(GT_position(:, plot_var_xy), 'b--', 'LineWidth', 2); % Ground Truth
+plot(X_est(:, plot_var_xy), 'r', 'LineWidth', 2);         % Estimated Position
+ylabel('Position');
+legend({'GT Position', 'X Est'}, 'Location', 'best');
+
+% Compute the RMSE
+error_x = (GT_position(:, 1) - X_est(:, 1)).^2;
+error_y = (GT_position(:, 2) - X_est(:, 2)).^2;
+error_theta = (GT_heading - X_est(:, 3)).^2;
+
+error = [error_x, error_y, error_theta];
+
+% Plot the RMSE on the right y-axis
+yyaxis right;
+plot(error(:, plot_var_xy), 'k-', 'LineWidth', 2); % RMSE in black
+ylabel('RMSE');
+
+% Labels and title
+xlabel('Time Step');
+title('GT Position, Estimated Position, and RMSE');
+grid on;
